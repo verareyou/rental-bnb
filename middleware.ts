cacbf04304ec5cc966abba32aa2b32bc93c03011
himanshu suthar <himanshuforpro@gmail.com>
2024-04-09 01:03:50 +0530

    Finished fondling.

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 export { default } from "next-auth/middleware"
 
-export const config = { 
+export const config = {
   matcher: [
     "/trips",
     "/reservations",
--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   },
   images: {
     domains: [
-      'res.cloudinary.com', 
+      'res.cloudinary.com',
       'avatars.githubusercontent.com',
       'lh3.googleusercontent.com'
     ]
